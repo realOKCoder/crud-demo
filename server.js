a7dc1071a77c843198964db4092a96fad11d8aec
Frazier Rodgers <frazierr04@gmail.com>
2022-06-09 17:37:15 -0500

    trying john's server file

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const express = require('express')
 const bodyParser = require('body-parser')
 const app = express()
 const MongoClient = require('mongodb').MongoClient
-const connectionString = 'mongodb+srv://fraz:fraz@cluster0.l7eo0jt.mongodb.net/?retryWrites=true&w=majority'
+const connectionString = 'mongodb+srv://tehuberjohn:MBxL6zKPAqCesB6X@cluster0.ceh9lz0.mongodb.net/?retryWrites=true&w=majority'
 
 MongoClient.connect(connectionString, { useUnifiedTopology: true })
     .then(client => {
